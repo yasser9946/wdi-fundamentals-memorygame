d15f66dda6a2edda2e22d7579746c1be70816e0b
yasser9946 <47639135+yasser9946@users.noreply.github.com>
2019-02-19 07:53:38 +0300

    Add files via upload

---
+++ wdi-fundamentals-memorygame/memory_game/All memory_game files/js/main.js
@@ -0,0 +1,12 @@
+console.log("Up and running!");
+
+console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king","king"];
+
+ var cardInPlay= [];
+
+ var cardOne;
+ cardOne = cards[0];
+ cardInPlay.push(cardOne);
+
+console.log("User flipped " + cardInPlay);
